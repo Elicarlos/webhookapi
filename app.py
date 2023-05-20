ed8754d57865c33aab994bd0a1ecc10df0916944
Elicarlos <elicarlosantos_@hotmail.com>
2023-05-20 02:42:57 -0300

    ajustes

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, redirect, url_for, request, jsonify
 app = Flask(__name__)
 @app.route('/api/', methods=['POST', 'GET'])
 def api():
-    data = request.data
-    print(dir(data))
+    data = request.get_json(data)
+ 
     print(data)
 
     return 'op'

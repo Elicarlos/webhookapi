ca21f4e0926d2b7fc48bf7f08c0bd568fd060f64
Elicarlos <elicarlosantos_@hotmail.com>
2023-05-20 01:37:37 -0300

    ajuste de api

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 
 from flask import Flask, redirect, url_for, request
 
-import requests
-
 app = Flask(__name__)
-
-
-
-
 @app.route('/api/', methods=['POST', 'GET'])
 def api():  
-   
     return 'Ola mundo'
 
 

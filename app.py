7be796115b9088a43cb089bb763c6a2e7c3959cb
Elicarlos <elicarlosantos_@hotmail.com>
2023-05-20 01:30:32 -0300

    ajuste de api

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ app = Flask(__name__)
 
 
 
-@app.route('/webhook.cajuinacode', methods=['POST', 'GET'])
-def hello_world():
-    response = requests.get('https://webhook.site/e284cee9-0278-4c23-9a8f-23d5192c08e2')
-    # print(dir(response))
-    print(response.text)
+@app.route('/api/', methods=['POST', 'GET'])
+def api():  
+   
     return 'Ola mundo'
 
 

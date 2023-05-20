8b7c786e12f52dfc9e6291efffd387159e0b0d9c
Elicarlos <elicarlosantos_@hotmail.com>
2023-05-20 02:59:42 -0300

    ajustes

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
 
 from flask import Flask, redirect, url_for, request, jsonify
 
+from flask_peewee.auth import Auth
+from flask_peewee.db import Database
+from peewee import *
+
+# configure our database
+DATABASE = {
+    'name': 'api.db',
+    'engine': 'peewee.SqliteDatabase',
+}
+
+
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+db = Database(app)
+
 @app.route('/api/', methods=['POST', 'GET'])
 def api():
-    data = request.json
+    data = request
  
     print(data)
 
     return 'op'
 
-
+auth = Auth(app, db)
 
 if __name__ == '__main__':
+    auth.User.create_table(fail_silently=True)
     app.run(debug=False)
 
 
